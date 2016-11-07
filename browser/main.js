6c712ad9729b6e178f40dacf1b77364d3bd6b631
rjisoo <jisoo.cc@gmail.com>
2016-11-07 16:39:38 -0500

    Account page #65 #82

--- browser/main.js
+++ browser/main.js
@@ -1,26 +1,26 @@
 // Libraries
-import React from 'react';
-import { render } from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'
 
 // React components
 import App from './components/App'
-import AllProductsContainer from './components/products/AllProductsContainer';
-import ProductContainer from './components/product/ProductContainer';
-import CartContainer from './components/cart/CartContainer';
-import OrderFormContainer from './components/orderform/OrderFormContainer';
-import OrderConfirmationContainer from './components/confirmation/OrderConfirmationContainer';
+import AllProductsContainer from './components/products/AllProductsContainer'
+import ProductContainer from './components/product/ProductContainer'
+import CartContainer from './components/cart/CartContainer'
+import OrderFormContainer from './components/orderform/OrderFormContainer'
+import OrderConfirmationContainer from './components/confirmation/OrderConfirmationContainer'
 import AccountContainer from './components/account/AccountContainer';
 import PersonalInfo from './components/account/PersonalInfo';
 
 
 // Redux actions and thunks
-import store from './store';
-import { fetchProducts } from './redux/products';
-import { fetchProduct } from './redux/product';
-import { fetchOrder } from './redux/order';
-import { fetchCart } from './redux/cart';
+import store from './store'
+import { fetchProducts } from './redux/products'
+import { fetchProduct } from './redux/product'
+import { fetchOrder } from './redux/order'
+import { fetchCart } from './redux/cart'
 import { fetchAccount } from './redux/account';
 
 const appEnter = () => store.dispatch(fetchProducts());
